27897ceb3023ede4960f43c40944a1eb8e5d4052
韩鑫 <hanxinimm@163.com>
2019-07-18 15:16:06 +0800

    升级客户端包版本，增加配置注入

---
+++ Hunter.NATS.Client/NATSServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Hunter.NATS.Client;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class NATSServiceCollectionExtensions
+    {
+        public static void AddNATSServer(this IServiceCollection services, IConfigurationRoot configuration)
+        {
+            services.Configure<NATSOptions>(options => configuration.GetSection("NATSOptions").Bind(options));
+        }
+
+        public static void AddNATSServer(this IServiceCollection services, IConfigurationRoot configuration, string clientId)
+        {
+            services.Configure<NATSOptions>(options =>
+            {
+                configuration.GetSection("NATSOptions").Bind(options);
+                options.ClientId = clientId;
+            });
+        }
+    }
+}
---
+++ Hunter.STAN.Client/STANServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿
+using Hunter.STAN.Client;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class STANServiceCollectionExtensions
+    {
+        public static void AddSTANServer(this IServiceCollection services, IConfigurationRoot configuration)
+        {
+            services.Configure<STANOptions>(options => configuration.GetSection("STANOptions").Bind(options));
+        }
+
+        public static void AddSTANServer(this IServiceCollection services, IConfigurationRoot configuration, string clientId)
+        {
+            services.Configure<STANOptions>(options =>
+            {
+                configuration.GetSection("STANOptions").Bind(options);
+                options.ClientId = clientId;
+            });
+        }
+    }
+}
