2f0ce4ebd432669d3930f9e2ed3447776176c928
韩鑫 <hanxinimm@163.com>
2019-11-05 09:15:30 +0800

    shengjidaima

--- Hunter.NATS.Client/NATSClient.cs
+++ Hunter.NATS.Client/NATSClient.cs
@@ -4,21 +4,14 @@ using DotNetty.Handlers.NATS;
 using DotNetty.Transport.Bootstrapping;
 using DotNetty.Transport.Channels;
 using DotNetty.Transport.Channels.Sockets;
-using System;
-using System.Collections.Generic;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
-using System.Collections.Concurrent;
-using System.Text.RegularExpressions;
-using DotNetty.Buffers;
-using System.Threading;
-using System.Linq;
 using Microsoft.Extensions.Options;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Hunter.NATS.Client
 {
-    public class NATSClient
+    public partial class NATSClient
     {
         /// <summary>
         /// NATS配置
@@ -91,134 +84,5 @@ namespace Hunter.NATS.Client
         }
 
         public bool IsOpen => _channel?.Open ?? false;
-
-        public async Task ContentcAsync()
-        {
-            if (!_options.ClusterNodes.Any())
-            {
-                IPHostEntry hostInfo = Dns.GetHostEntry(_options.Host);
-                _options.ClusterNodes.AddRange(hostInfo.AddressList.Select(v => new IPEndPoint(v, _options.Port)));
-            }
-
-            var ClusterNode = _options.ClusterNodes.First();
-
-            _channel = await _bootstrap.ConnectAsync(ClusterNode);
-
-            _info = await ConnectAsync();
-        }
-
-        private async Task<InfoPacket> ConnectAsync()
-        {
-
-            var Packet = new ConnectPacket(true, false, false, null, null, _clientId, null);
-
-            _infoTaskCompletionSource = new TaskCompletionSource<InfoPacket>();
-
-            await _channel.WriteAndFlushAsync(Packet);
-
-            var ConnectInfoResult = await _infoTaskCompletionSource.Task;
-
-            return ConnectInfoResult;
-
-        }
-
-        public async Task<string> SubscribeAsync(string subject, string queueGroup, Action<NATSMsgContent> handler, string subscribeId = null)
-        {
-            var SubscribeId = subscribeId ?? $"sid{Interlocked.Increment(ref _subscribeId)}";
-
-            _localSubscriptionConfig[SubscribeId] = new NATSSubscriptionConfig(subject, SubscribeId, handler);
-
-            var SubscribePacket = new SubscribePacket(SubscribeId, subject, queueGroup);
-
-            await _channel.WriteAndFlushAsync(SubscribePacket);
-
-            return SubscribeId;
-
-        }
-
-        public async Task<string> SubscribeAsync(string subject, string queueGroup, Action<NATSMsgContent> handler)
-        {
-            var SubscribeId = $"sid{Interlocked.Increment(ref _subscribeId)}";
-
-            _localSubscriptionConfig[SubscribeId] = new NATSSubscriptionConfig(subject, SubscribeId, handler);
-
-            var SubscribePacket = new SubscribePacket(SubscribeId, subject, queueGroup);
-
-            await _channel.WriteAndFlushAsync(SubscribePacket);
-
-            return SubscribeId;
-
-        }
-
-        public async Task UnSubscribeAsync(string subscribeId)
-        {
-            if (_localSubscriptionConfig.Remove(subscribeId))
-            {
-                var UnSubscribePacket = new UnSubscribePacket(subscribeId);
-
-                await _channel.WriteAndFlushAsync(UnSubscribePacket);
-            }
-        }
-
-        public async Task AutoUnSubscribeAsync(string subscribeId, int max_msgs)
-        {
-            if (_localSubscriptionConfig.TryGetValue(subscribeId, out var subscriptionConfig))
-            {
-                subscriptionConfig.MaxProcessed = max_msgs;
-
-                var UnSubscribePacket = new UnSubscribePacket(subscribeId, max_msgs);
-
-                await _channel.WriteAndFlushAsync(UnSubscribePacket);
-            }
-        }
-
-        /// <summary>
-        /// 异步发送
-        /// </summary>
-        /// <param name="subject">主体</param>
-        /// <param name="data">数据</param>
-        /// <returns></returns>
-        public Task PublishAsync(string subject, byte[] data)
-        {
-            var Packet = new PublishPacket(subject, data);
-
-            return _channel.WriteAndFlushAsync(Packet);
-        }
-
-
-        public Task PingAsync()
-        {
-            var Packet = new PingPacket();
-
-            return _channel.WriteAndFlushAsync(Packet);
-        }
-
-        public Task PongAsync()
-        {
-            var Packet = new PongPacket();
-
-            return _channel.WriteAndFlushAsync(Packet);
-        }
-
-        protected void InfoAsync(InfoPacket info)
-        {
-            _infoTaskCompletionSource.TrySetResult(info);
-        }
-
-        protected void MessageAsync(MessagePacket message)
-        {
-            if (_localSubscriptionConfig.TryGetValue(message.SubscribeId, out var subscriptionConfig))
-            {
-                if (subscriptionConfig.MaxProcessed.HasValue)
-                {
-                    subscriptionConfig.MaxProcessed--;
-
-                    if (subscriptionConfig.MaxProcessed <= 0)
-                        _localSubscriptionConfig.Remove(message.SubscribeId);
-                }
-
-                subscriptionConfig.Handler(new NATSMsgContent(message.SubscribeId, message.Subject, message.ReplyTo, message.Payload));
-            }
-        }
     }
 }
