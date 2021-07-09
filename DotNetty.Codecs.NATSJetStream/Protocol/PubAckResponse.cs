9215ec5cf96c92b2d67eff8fcce5086263871fa9
韩鑫 <hanxinimm@163.com>
2021-07-09 10:01:29 +0800

    完善代码逻辑，提升连接效率

---
+++ DotNetty.Codecs.NATSJetStream/Protocol/PubAckResponse.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetty.Codecs.NATSJetStream.Protocol
+{
+    public class PubAckResponse : JetStreamResponse
+    {
+        [JsonProperty("stream")]
+        public string Stream { get; set; }
+
+        [JsonProperty("seq")]
+        public long Sequence { get; set; }
+
+        [JsonProperty("duplicate")]
+        public bool Duplicate { get; set; }
+    }
+}
---
+++ Hunter.NATS.Client/JetStream/PublishOptions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hunter.NATS.Client.JetStream
+{
+    public class PublishOptions
+    {
+        public TimeSpan ttl { get; set; }
+
+        /// <summary>
+        /// 消息编号
+        /// </summary>
+        public string MessageId { get; set; }
+        /// <summary>
+        /// 预期的消息编号
+        /// </summary>
+        /// <remarks>
+        /// Expected last msgId
+        /// </remarks>
+        public string ExpectedMessageId { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <remarks>
+        /// Expected stream name
+        /// </remarks>
+        public string ExpectedStreamName { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <remarks>
+        /// Expected last sequence
+        /// </remarks>
+        public long ExpectedSequence { get; set; }
+    }
+}
